721061e123d00938b82f9490e5c52a050ea5cedb
AlucardxTepes <alucard.557@gmail.com>
2017-04-25 15:41:08 -0400

    Add dynamic gif call to Gif detail route

--- src/main/java/com/alucard/giflib/controller/GifController.java
+++ src/main/java/com/alucard/giflib/controller/GifController.java
@@ -1,6 +1,12 @@
 package com.alucard.giflib.controller;
 
+import com.alucard.giflib.data.GifRepository;
+import com.alucard.giflib.model.Gif;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,9 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class GifController {
 
+  @Autowired
+  private GifRepository gifRepository;
+
   @RequestMapping("/")
   public String listGifs() {
     return "home";
+  }
+
+  @RequestMapping("/gif/{name}")
+  public String gifDetails(@PathVariable String name, ModelMap modelMap){
+    Gif gif = gifRepository.findByName(name);
+    modelMap.put("gif", gif);
+    return "gif-details";
   }
 
 }
